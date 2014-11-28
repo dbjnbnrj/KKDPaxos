d4c9401027582de5ed86b5e271effda0b03d1ed6
root <root@euca-10-2-128-17.eucalyptus.internal>
2014-11-28 15:14:34 -0500

    Adding Paxos Phase 1

--- paxos-2.py
+++ paxos-2.py
@@ -3,42 +3,60 @@ import SocketServer
 import sys
 import logging
 import socket
+from Queue import Queue
+
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(name)s: %(message)s',
                     )
 
+class Messenger:
+	def __init__(self, owner):
+		self.owner = owner
+
+	def recv(self, data):
+		print('messenger recieves data',data)
+		if "PREPARE" in data:
+			b = data.split()
+			self.owner.send_promise(int(b[1]))
+		if "ACK" in data:
+			s = data.split()
+			self.owner.recieve_ack( int(s[1]), int(s[2]), int(s[3]) ) 
+		
+
 class PaxosRequestHandler(SocketServer.BaseRequestHandler):
     
     def __init__(self, request, client_address, server):
         self.logger = logging.getLogger('PaxosRequestHandler')
-        self.logger.debug('__init__')
+        print('__init__')
         SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
         return
 
     def setup(self):
-        self.logger.debug('setup')
+        print('setup')
         return SocketServer.BaseRequestHandler.setup(self)
 
     def handle(self):
-        self.logger.debug('handle')
+        print('handle')
 
         # Echo the back to the client
         data = self.request.recv(1024)
-        self.logger.debug('recv()->"%s"', data)
+        print('recv()->"%s"', data)
+	self.server.owner.messenger.recv(data)
         self.request.send(data)
         return
 
     def finish(self):
-        self.logger.debug('finish')
+        print('finish')
         return SocketServer.BaseRequestHandler.finish(self)
 
 
 class PaxosServer(SocketServer.TCPServer):
     
-    def __init__(self, server_address, handler_class=PaxosRequestHandler):
+    def __init__(self, owner, server_address, handler_class=PaxosRequestHandler):
         SocketServer.TCPServer.__init__(self, server_address, handler_class)
-        return
+        self.owner = owner
+	return
 
     def server_activate(self):
         print('server_activate')
@@ -75,51 +93,83 @@ class PaxosServer(SocketServer.TCPServer):
         print('close_request(%s)', request_address)
         return SocketServer.TCPServer.close_request(self, request_address)
 
-#class PaxosClient:
+class PaxosClient:
+	def __init__(self, address, data):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    		s.connect(address)
+		 
+   		 # Send the data
+		print('sending msg %s', data)
+    		len_sent = s.send(data)
+
+    		# Receive a response
+    		print('waiting for response')
+    		#response = s.recv(len_sent)
+    		#print('response from server: "%s"', response)
+
+    		# Clean up
+    		print('closing socket')
+    		s.close()
 
 class Node(threading.Thread):
 	def __init__(self, addr, port):
 		threading.Thread.__init__(self)
 		self.address= (addr, port)
-		self.server = PaxosServer(self.address, PaxosRequestHandler)
-	
+		self.server = PaxosServer(self, self.address, PaxosRequestHandler)
+		self.q = Queue()
+		self.messenger = Messenger(self)
+		self.ballotNum = 0
+		self.aBallotNum = 0
+		self.acceptedNum = 0
+		self.acceptedVal = 0
+		self.highestBallot = 0
+		self.highestValue = 0
+		self.currentValue = ""
+		self.acks = 0
+		self.quorum = 1
+
 	def run(self):
-		logger.debug('Running Node')
+		print('running node')
 		t = threading.Thread(target=self.server.serve_forever)
 		t.setDaemon(True)
 		t.start()
+		while True:
+			self.currentValue = self.q.get()
+			self.send_propose()
+			self.q.task_done()
 
-
+	def send_propose(self):
+		self.ballotNum += 1
+		msg = "PREPARE: "+str(self.ballotNum)
+		print self.server.server_address
+		client = PaxosClient(self.server.server_address, msg)
+	
+	def send_promise(self, b):
+		if b >= self.aBallotNum:
+			self.aBallotNum = b
+		msg = "ACK {0} {1} {2}".format(b,self.acceptedNum,self.acceptedVal)
+		print self.server.server_address
+		client = PaxosClient(self.server.server_address, msg)
+	
+	def recieve_ack(self, BallotNum, b, val):
+		if( BallotNum == self.ballotNum):
+			self.acks += 1
+			val = 0
+			if(self.highestBallot < b):
+				self.highestBallot = b
+				self.highestValue = val
+			if(self.acks >= self.quorum):
+				if self.highestValue != None:
+					self.currentValue = str(self.highestValue)
+				self.acks = 0
+				msg = "ACCEPT {0} {1}".format(BallotNum, self.currentValue)
+				client = PaxosClient(self.server.server_address, msg)
+		
 
 if __name__ == '__main__':
 
     addr = 'localhost'
     port = 0
     n = Node(addr, port)
+    n.q.put("deposit")
     n.start()
-    ip, port = n.server.server_address 
-    logger = logging.getLogger('client')
-    logger.info('Server on %s:%s', ip, port)
-
-    # Connect to the server
-    logger.debug('creating socket')
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    logger.debug('connecting to server')
-    s.connect((addr, port))
-
-    # Send the data
-    message = 'Hello, world'
-    logger.debug('sending data: "%s"', message)
-    len_sent = s.send(message)
-
-    # Receive a response
-    logger.debug('waiting for response')
-    response = s.recv(len_sent)
-    logger.debug('response from server: "%s"', response)
-
-    # Clean up
-    logger.debug('closing socket')
-    s.close()
-    logger.debug('closing node')
-    n.server.socket.close()
-    logger.debug('done')
