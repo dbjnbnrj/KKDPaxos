4a8dce9d2407d03a21bfab1d8fb113af8c93872d
KK <micang.tw@gmail.com>
2014-11-29 00:21:35 -0800

    Add client TCP server. (which receives user commands)

--- balanceSystem.py
+++ balanceSystem.py
@@ -12,9 +12,12 @@ class BalanceSystem():
         self.paxosNode = paxos.PaxosNode(self.pid, 
                             self.balanceMgr.processConsensus,
                             self.balanceMgr.getLogItem)
+        self.balanceMgr.setPaxosNode(self.paxosNode)
+
+        address = ("localhost", 9999)
+        self.clientTCP = clientInterface.ClientTCPServer(self.balanceMgr, address)
         self.cli = clientInterface.CLI(self.balanceMgr)
 
-        self.balanceMgr.setPaxosNode(self.paxosNode)
 
 
     def determinePid(self):
@@ -23,6 +26,7 @@ class BalanceSystem():
 
     def run(self):
         """ Start balance system. """
+        self.clientTCP.start()
         self.cli.cmdloop()
 
 if(__name__ == "__main__"):
--- clientInterface.py
+++ clientInterface.py
@@ -1,9 +1,23 @@
 import cmd
-import string, sys
+import SocketServer 
+import threading
 
 class CLI(cmd.Cmd):
+    """ This class implements command line interface of balance system. 
+    
+    CAUTION: Do not use print() in this class. Please use self.stdout.write() instead.
 
+    Valid commands:
+        - deposit [val]
+        - withdraw [val]
+        - balance
+        - fail
+        - unfail
+        - quit, q, ctrl+d
+    """
+    
     def __init__(self, balanceMgr):
+        """ Initiate a balance command line interface. """
         cmd.Cmd.__init__(self)
         self.prompt = '> '
         self._balanceMgr = balanceMgr
@@ -23,12 +37,12 @@ class CLI(cmd.Cmd):
             return
         
         self._balanceMgr.deposit(val)
-        print "Depositing ",arg, "!"
-
+        self.stdout.write("Depositing {0}!\n".format(val))
 
     def help_deposit(self):
-        print "syntax: deposit [amount]",
-        print "-- = deposits amount in the bank"
+        msg = "syntax: deposit [amount]\n"
+        msg += "\t-- = deposits amount in the bank\n"
+        self.stdout.write(msg)
 
     # -------------------
     def do_withdraw(self, arg):
@@ -43,44 +57,115 @@ class CLI(cmd.Cmd):
             return
         
         self._balanceMgr.withdraw(val)
-        print "Withdraw ", arg, "!"
+        self.stdout.write("Withdraw {0}!\n".format(val))
 
     def help_withdraw(self):
-        print "syntax: withdraw [amount]",
-        print "-- = withdraws amount in the bank"
+        msg = "syntax: withdraw [amount]\n"
+        msg += "\t-- = withdraws amount in the bank\n"
+        self.stdout.write(msg)
 
     # -------------------
     def do_balance(self, arg):
         balance = self._balanceMgr.getBalance()
-        print "Balance: ", balance, "!"
+        self.stdout.write("Balance: {0}!\n".format(balance))
 
     def help_balance(self):
-        print "syntax: balance",
-        print "-- = gets User Balance"
+        msg = "syntax: balance\n"
+        msg += "\t-- = gets user balance\n"
+        self.stdout.write(msg)
 
     # -------------------
     def do_fail(self):
-        print "Fail"
+        self.stdout.write("Fail\n")
 
     def help_fail(self):
-        print "syntax: fail",
-        print "-- = Simulate a server failure"
+        msg = "syntax: fail\n"
+        msg += "\t-- = simulate a server failure\n"
+        self.stdout.write(msg)
 
     # -------------------
     def do_unfail(self):
-        print "Unfail"
+        self.stdout.write("Unfail\n")
 
     def help_unfail(self):
-        print "syntax: unfail",
-        print "-- = resume from failure"
+        msg = "syntax: unfail\n"
+        msg += "\t-- = resume from failure\n"
+        self.stdout.write(msg)
 
     # -------------------
     def do_quit(self, arg):
-        sys.exit(1)
+        self.stdout.write("\nSee you ~\n")
+        return True
 
     def help_quit(self):
-        print "syntax: quit",
-        print "-- terminates the application"
+        msg = "syntax: quit\n"
+        msg += "\t-- = terminates the application\n"
+        self.stdout.write(msg)
 
     # shortcuts
     do_q = do_quit
+    do_EOF = do_quit
+
+
+class ClientRequestHandler(SocketServer.BaseRequestHandler):
+    """
+    The RequestHandler class for ClientTCPServer.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+    class FakeStdio():
+        """ This is a fake stdio class that will pass to cmd_processor. 
+        
+        The only important function is write(). cmd_processor will call its 
+        stdout.write() to output messages.
+        """
+        def __init__(self, socket):
+            self.socket = socket
+        def write(self, msg):
+            self.socket.sendall(msg)
+    
+    def handle(self):
+        # create fake stdio
+        fakeStdio = ClientRequestHandler.FakeStdio(self.request)
+        # get command processor
+        cmd_processor = self.server.cmd_processor
+        # redirect stdout
+        cmd_processor.stdout = fakeStdio
+
+        # get date from client
+        data = self.request.recv(1024).strip()
+        # process command
+        cmd_processor.onecmd(data)
+
+class ClientTCPServer(SocketServer.TCPServer):
+    """ A class that receives client commands from TCP.
+
+    This server will start a new thread to listen to incoming connection after 
+    executing start(). To stop the server, execute shutdown()
+
+    """
+    def __init__(self, balanceMgr, serverAddress):
+        """ Initiate a TCP server. 
+
+        Input:
+            balanceMgr : BalanceMgr
+            serverAddress : a tuple (ip, port)
+        """
+        # base class's constructor
+        SocketServer.TCPServer.__init__(self, serverAddress, ClientRequestHandler)
+        
+        # command processor will be used by ClientRequestHandler
+        # it parse the input and dispatch commands 
+        self.cmd_processor = CLI(balanceMgr)
+        
+        # thread that runs self.serve_forever
+        self.thread = threading.Thread(target=self.serve_forever)
+        self.thread.setDaemon(True)
+
+    def start(self):
+        """ Create a new thread that listens to incoming connection. """
+        self.thread.start()
+
+    # To stop running, call ClientTCPServer.shutdown()
