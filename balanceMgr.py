cc0a8ce306dd41fbd6edb4f26c8fd71c481a3483
KK <micang.tw@gmail.com>
2014-11-29 22:32:28 -0800

    Finish LogMgr class and its test code.

--- balanceMgr.py
+++ balanceMgr.py
@@ -1,35 +1,116 @@
+import os
 import collections
+import threading
+import pickle
 
 DEPOSIT = 0
 WITHDRAW = 1
+LOGFILE = "balance_log.txt"
 
 LogItem = collections.namedtuple('LogItem', ['pid', 'time', 'type', 'amount'])
 
 class LogMgr():
     """ A class to handle log. 
 
-    Log should be written into disc to overcome failures. However, this 
-    implementation use main memory for simplicity.
+    Log should be written into disc to overcome failures.
     """
+  
+    # To check whether the file is corrupted or not, we add ENDING_STR at 
+    # the end of each line.
+    ENDING_STR = "OK"
     def __init__(self):
-        self.log = []
-        self.balance = 0
-
+        self._log = []
+        self._balance = 0 
+        
+        # Read log
+        self._recover()
+    
+    # -----
+    # Public functions
+    # -----
     def appendLog(self, item):
-        """ Append an item to log, then update balance. """
-        pass
+        """ Append an item to log, then update balance. 
+        
+        Input argument:
+            item: LogItem
+        """
+        # Calculate result balance
+        balance = self._balance
+        if(item.type == DEPOSIT):
+            balance += item.amount
+        elif(item.type == WITHDRAW):
+            balance -= item.amount
 
+        # Create a message that will be written in log
+        itemStr = self._convertItem2String(item) 
+        msg = "{0};{1}".format(itemStr, balance)
+
+        # Update local log and balance
+        self._log.append(item)
+        self._balance = balance
+
+        # Write hard drive
+        with open(LOGFILE, 'a') as fp:
+            fp.write(msg+";"+self.ENDING_STR+"\n")
+            fp.flush()
+    
     def getBalance(self):
-        """ Return current balance. """
-        pass
+        """ Return current balance. 
+        
+        getBalance() -> int
+        """
+        return self._balance
 
-    def getNextIdx(self):
-        """ Return the next available index. """
-        pass
+    def getItem(self, idx):
+        """ Return the LogItem of given index. 
+       
+        getItem() -> LogItem
 
-    def __getitem__(self, idx):
-        """ Overload operator []. Return the item of given index. """
-        pass
+        Return None if there is no item of given index. It's expected that when a 
+        caller aquires an item, the item is already in the log.
+        """
+        if(idx < len(self._log)):
+            return self._log[idx]
+        else:
+            return None
+
+    # -----
+    # Private functions
+    # -----
+    def _recover(self):
+        """ Read log from hard drive. """
+        # Check if file exists
+        if(not os.path.isfile(LOGFILE)):
+            return
+        
+        # Read file
+        with open(LOGFILE, 'r') as fp:
+            lines = [ line.strip() for line in fp.readlines() ]
+        
+        # Check if there is corrupted line
+        # We can get balance from the latest line
+        lastValidLine = ""
+        for idx, line in enumerate(lines):
+            if(not line.endswith(LogMgr.ENDING_STR)):
+                # If it's corrupted, then stop reading log
+                print "Loading log... Line {0} is corrupted.".format(idx+1)
+                break
+            else:
+                # Else, add item to log
+                itemStr = line.split(';')[0]
+                self._log.append(self._convertString2Item(itemStr))
+                lastValidLine = line
+        
+        # Get balance
+        balanceStr = lastValidLine.split(';')[1]
+        self._balance = int(balanceStr)            
+
+    def _convertItem2String(self, item):
+        return " ".join([str(v) for v in item])
+     
+    def _convertString2Item(self, itemStr):
+        tokens = itemStr.split()
+        return LogItem(int(tokens[0]), int(tokens[1]), int(tokens[2]), int(tokens[3])) 
 
 class BalanceMgr():
     """ Class for managing balance.
---
+++ test/test_logmgr.py
@@ -0,0 +1,91 @@
+import os, sys
+import unittest
+from cStringIO import StringIO
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append( os.path.dirname(this_dir) )
+
+from balanceMgr import LogMgr, LOGFILE, LogItem, DEPOSIT, WITHDRAW
+
+class TestSequenceFunctions(unittest.TestCase):
+    
+    def setUp(self):
+        """ This function will be called before each test. """
+        try:
+            os.remove(LOGFILE)
+        except:
+            pass
+        self.logmgr = LogMgr()
+
+        # Redirect stdout
+        self.held, sys.stdout = sys.stdout, StringIO()
+
+    def test_recover(self):
+        # Create log file
+        balance = 987
+        items = [LogItem(1,2,3,4), LogItem(5,6,7,8)] 
+        lines = ["1 2 3 4;123", "5 6 7 8;{0}".format(balance)]
+        with open(LOGFILE, 'w') as fp:
+            for line in lines:
+                fp.write(line+';'+LogMgr.ENDING_STR+"\n")
+            fp.flush()
+        
+        # Create logMgr and automatically read log
+        logmgr = LogMgr()
+        for idx, item in enumerate(items):
+            # check log content
+            self.assertEquals(logmgr.getItem(idx), item)
+        
+        # check getBalance
+        self.assertEquals(logmgr.getBalance(), balance)
+
+    def test_recover_corrupted(self):
+        # Create log file
+        balance = 987
+        items = [LogItem(1,2,3,4), LogItem(5,6,7,8)]
+        lines = ["1 2 3 4;123", "5 6 7 8;{0}".format(balance)]
+        with open(LOGFILE, 'w') as fp:
+            for line in lines:
+                fp.write(line+';'+LogMgr.ENDING_STR+"\n")
+            fp.write("corrupted line;22\n")
+            fp.write("good line;344"+';'+LogMgr.ENDING_STR+"\n")
+            fp.flush()
+       
+        # Create logMgr and automatically read log
+        logmgr = LogMgr()
+       
+        # check items
+        self.assertEquals(logmgr._log, items)
+        
+        # check getBalance
+        self.assertEquals(logmgr.getBalance(), balance)
+
+        # check msg
+        self.assertEqual(sys.stdout.getvalue(), 'Loading log... Line 3 is corrupted.\n')
+
+    def test_append(self):
+        balance = 20-8
+        items = [LogItem(1,2,DEPOSIT,20), LogItem(5,6,WITHDRAW,8)]
+        for item in items:
+            self.logmgr.appendLog(item)
+
+        # check items
+        self.assertEquals(self.logmgr._log, items)
+
+        # check balance
+        self.assertEquals(self.logmgr.getBalance(), balance)
+
+        
+
+#    def test_choice(self):
+#        element = random.choice(self.seq)
+#        self.assertTrue(element in self.seq)
+#
+#    def test_sample(self):
+#        with self.assertRaises(ValueError):
+#            random.sample(self.seq, 20)
+#        for element in random.sample(self.seq, 5):
+#            self.assertTrue(element in self.seq)
+
+if __name__ == '__main__':
+    unittest.main()
