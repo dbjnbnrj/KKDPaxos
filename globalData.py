8e6f118cb383aec57a5af5651c05916818bb3e46
root <root@ip-172-30-0-132.ec2.internal>
2014-12-09 02:42:31 +0000

    updating global data

--- globalData.py
+++ globalData.py
@@ -4,10 +4,10 @@ import collections
 Address = collections.namedtuple('Address', ['ip', 'port', 'cmdTCPPort'])
 
 ADDRESS = [Address(ip="54.86.219.155", port=9990, cmdTCPPort=9999),
-           Address(ip="54.165.53.186", port=9990, cmdTCPPort=9999),
-           Address(ip="54.165.54.129", port=9990, cmdTCPPort=9999),
-           Address(ip="54.174.99.172", port=9990, cmdTCPPort=9999),
-           Address(ip="54.174.181.8", port=9990, cmdTCPPort=9999)]
+           Address(ip="54.149.109.117", port=9990, cmdTCPPort=9999),
+           Address(ip="54.77.248.101", port=9990, cmdTCPPort=9999),
+           Address(ip="54.169.73.215", port=9990, cmdTCPPort=9999),
+           Address(ip="54.94.231.250", port=9990, cmdTCPPort=9999)]
 
 #CMD_TCP_PORT = 9999
 #ADDRESS = [Address(ip="localhost", port=9990, cmdTCPPort=CMD_TCP_PORT),
