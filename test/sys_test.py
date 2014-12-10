2ed4dbb7ffb023ef41c518b77a5dcdc8aff0c0e1
KK <micang.tw@gmail.com>
2014-12-09 23:01:38 -0800

    modify sys_test to deposit-only

--- test/sys_test.py
+++ test/sys_test.py
@@ -11,7 +11,8 @@ ADDRESS = [Address(ip="54.86.219.155", port=9990, cmdTCPPort=9999),
            Address(ip="54.94.231.250", port=9990, cmdTCPPort=9999)]
 
 
-command = ["deposit", "withdraw"]
+#command = ["deposit", "withdraw"]
+command = ["deposit"]
 
 
 
@@ -21,17 +22,17 @@ def worker(address):
     
     # Connect to server and send data
     
-    for dummy in range(30):
+    for dummy in range(10):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(address)
-        msg = "{0} {1}".format(command[randint(0,1)], randint(1,100))
+        msg = "{0} {1}".format(command[randint(0,len(command)-1)], randint(1,100))
         print dummy, msg
         sock.sendall(msg)
         sock.recv(1024)
         sock.close()
 
 threads = []
-for i in range(4):
+for i in range(5):
     addr = (ADDRESS[i].ip, ADDRESS[i].cmdTCPPort)
     t = threading.Thread(target=worker, args=(addr,))
     threads.append(t)
